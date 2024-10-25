5c906826af5a4522132483f81a28a15cc1a72f47
Umer Saleem <162819866+UmerSaleemUltra@users.noreply.github.com>
2024-10-25 08:03:33 -0700

    update apis

--- routes/products.js
+++ routes/products.js
@@ -1,34 +1,21 @@
-import express from 'express';
-import Products from '../models/Products.js';
+import express from 'express'
+import Products from '../models/Products.js'
 
-const app = express();
-app.use(express.json()); // Middleware to parse JSON data
+const router = express.Router()
 
-// GET route for fetching all products
-app.get('/', async (req, res) => {
-  try {
-    const data = await Products.find();
-    res.status(200).send({ message: 'Products fetched successfully!', data });
-  } catch (error) {
-    console.error('Error fetching products:', error);
-    res.status(500).send({ message: 'Error fetching products' });
-  }
-});
+router.get('/', async (req, res) => {
+  const data = await Products.find()
+  res.send({ message: 'Products fetched successfully!', data })
+})
 
-// POST route for adding a new product
-app.post('/addProduct', async (req, res) => {
-  try {
-    const data = req.body;
-    await Products.create(data);
-    res.status(201).send({ message: 'Product added successfully!' });
-  } catch (error) {
-    console.error('Error adding product:', error);
-    res.status(500).send({ message: 'Error adding product' });
-  }
-});
+router.post('/addProduct', async (req, res) => {
+  const data = req.body
+  await Products.create(data)
+  res.send({ message: 'Product added successfully!' })
+})
 
-// Export the app for Vercel serverless
-export default app;
+export default router
+
 
 /*
 fetch('http://localhost:3001/products/addProduct', {
@@ -36,7 +23,11 @@ fetch('http://localhost:3001/products/addProduct', {
     headers: {
         "Content-Type": "application/json"
     },
-   
+    body: JSON.stringify({
+        title: 'Wow',
+        description: 'test',
+        price: 50
+    })
 }).then(res => res.json())
 .then(res => console.log(res))
 
